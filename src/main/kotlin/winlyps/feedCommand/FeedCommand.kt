ce8cb7525711bdf34c427379fc39ef388bb53948
Winlypse <nts3@o2.pl>
2024-10-20 13:34:01 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/feedCommand/FeedCommand.kt
@@ -0,0 +1,14 @@
+package winlyps.feedCommand
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class FeedCommand : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/kotlin/winlyps/feedCommand/FeedCommandExecutor.kt
@@ -0,0 +1,4 @@
+package winlyps.feedCommand
+
+class FeedCommandExecutor {
+}
