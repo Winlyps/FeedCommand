f1a05b43333aef690e8bf697bd4f247ece2709b4
Winlypse <nts3@o2.pl>
2024-10-20 13:34:10 +0200

    Changes!

--- src/main/kotlin/winlyps/feedCommand/FeedCommand.kt
+++ src/main/kotlin/winlyps/feedCommand/FeedCommand.kt
@@ -5,10 +5,13 @@ import org.bukkit.plugin.java.JavaPlugin
 class FeedCommand : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
+        // Register the feed command
+        getCommand("feed")?.setExecutor(FeedCommandExecutor(this))
+
+        logger.info("FeedCommand plugin has been enabled.")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        logger.info("FeedCommand plugin has been disabled.")
     }
 }
--- src/main/kotlin/winlyps/feedCommand/FeedCommandExecutor.kt
+++ src/main/kotlin/winlyps/feedCommand/FeedCommandExecutor.kt
@@ -1,4 +1,26 @@
 package winlyps.feedCommand
 
-class FeedCommandExecutor {
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+class FeedCommandExecutor(private val plugin: FeedCommand) : CommandExecutor {
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (sender !is Player) {
+            sender.sendMessage("Only players can use this command.")
+            return true
+        }
+
+        if (!sender.hasPermission("feedcommand.use") && !sender.isOp) {
+            sender.sendMessage("You do not have permission to use this command.")
+            return true
+        }
+
+        sender.foodLevel = 20
+        sender.saturation = 20f
+
+        return true
+    }
 }
